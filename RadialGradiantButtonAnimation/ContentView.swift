a7ce614d322a590c5f2170ed7e0ab3274e36c32f
Chris Gonzalez <christogonz92@gmail.com>
2024-07-28 16:37:54 +0200

    Initial Commit

---
+++ RadialGradiantButtonAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RadialGradiantButtonAnimation
+//
+//  Created by Christopher Gonzalez on 2024-07-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RadialGradiantButtonAnimation/RadialGradiantButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialGradiantButtonAnimationApp.swift
+//  RadialGradiantButtonAnimation
+//
+//  Created by Christopher Gonzalez on 2024-07-28.
+//
+
+import SwiftUI
+
+@main
+struct RadialGradiantButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
